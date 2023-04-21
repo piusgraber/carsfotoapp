a547b118d7aa1f5317c4a49ea63faa15432bac3e
Pius Graber <pg@webdata.ch>
2023-04-21 18:36:10 +0200

    Fetch Server

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -2,7 +2,8 @@ type FetchALead = (guid: string) => Promise<{}>;
 type FetchLeadsByRecallStatus = (type: number) => Promise<[]>;
 
 export const fetchALead: FetchALead = async guid => {
-    const url = "http://localhost:3344/recallLead?gid=" + guid + ""
+    const url = "https://api.car-ware.ch/recallLead?gid=" + guid + ""
+//    const url = "http://localhost:3344/recallLead?gid=" + guid + ""
     console.log(url);
 	const resp = await fetch(url);
     let leads = await resp.json();
@@ -13,7 +14,8 @@ export const fetchALead: FetchALead = async guid => {
 
 
 export const fetchLeadsByRecallStatus: FetchLeadsByRecallStatus = async type => {
-    const url = 'http://localhost:3344/recallLeads?type=' + type
+    const url = 'https://api.car-ware.ch/recallLeads?type=' + type
+    //const url = 'http://localhost:3344/recallLeads?type=' + type
     console.log(url);
     const resp = await fetch(url);
     let listeL = await resp.json();
